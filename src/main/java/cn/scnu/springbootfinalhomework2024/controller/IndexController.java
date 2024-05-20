7ff14df8c8524b0754098d52a000ebefb1f353b6
gugg <GuGGC1@outlook.com>
2024-05-20 20:57:57 +0800

    骂宿舍长

--- src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
+++ src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
+    @RequestMapping("/index") //hjx是傻逼
     public String index() {
         return "index";
     }
