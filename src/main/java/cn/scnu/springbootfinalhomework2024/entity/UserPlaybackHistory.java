e3e905e389de1ecdb735c14b89067406455f6f46
hjx <1751001978@qq.com>
2024-05-20 23:04:47 +0800

    更改实体类

---
+++ src/main/java/cn/scnu/springbootfinalhomework2024/entity/UserPlaybackHistory.java
@@ -0,0 +1,9 @@
+package cn.scnu.springbootfinalhomework2024.entity;
+
+import java.time.LocalDateTime;
+
+public class UserPlaybackHistory {
+    private int userId;
+    private int movieId;
+    private LocalDateTime viewingTimestamp;
+}
