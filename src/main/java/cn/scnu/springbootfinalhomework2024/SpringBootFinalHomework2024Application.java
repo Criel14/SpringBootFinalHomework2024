5594c50089f5025b07f3164f05519c858f442103
Criel14 <1298373079@qq.com>
2024-05-20 12:34:15 +0800

    add1

---
+++ src/main/java/cn/scnu/springbootfinalhomework2024/SpringBootFinalHomework2024Application.java
@@ -0,0 +1,13 @@
+package cn.scnu.springbootfinalhomework2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFinalHomework2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFinalHomework2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/scnu/springbootfinalhomework2024/SpringBootFinalHomework2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.scnu.springbootfinalhomework2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFinalHomework2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
