7a66ccc975a06d160ce168f8474229c5a48abb33
Criel14 <1298373079@qq.com>
2024-05-21 16:31:52 +0800

    修改了一点后端

--- src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
+++ src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -28,6 +29,7 @@ public class IndexController {
         }
         return "index";
     }
+
 
     @RequestMapping("/register")
     public String register(User user, Model model) {
