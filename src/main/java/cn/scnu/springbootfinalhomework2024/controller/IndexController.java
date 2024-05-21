9ea847d15080be40979b602587561f64aedaabd0
hjx <1751001978@qq.com>
2024-05-21 15:26:31 +0800

    登录注册初修改

--- src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
+++ src/main/java/cn/scnu/springbootfinalhomework2024/controller/IndexController.java
@@ -2,11 +2,15 @@ package cn.scnu.springbootfinalhomework2024.controller;
 
 
 import cn.scnu.springbootfinalhomework2024.entity.User;
+import cn.scnu.springbootfinalhomework2024.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    UserService userService;
 
     @RequestMapping("/index")
     public String index() {
@@ -14,9 +18,23 @@ public class IndexController {
     }
 
     @RequestMapping("/login")
-    public String login(User user){
+    public String login(User user, Model model){
         System.out.println(user);
-        return "redirect:/index";
+        User LoginUser = userService.ConFirmLogin(user);
+        if(LoginUser == null) return "index";
+        else{
+            model.addAttribute("user",LoginUser);
+            return "index";
+        }
+    }
+
+    @RequestMapping("/register")
+    public String register(User user,Model model){
+        String str = userService.register(user);
+        if(str == "注册成功"){
+            model.addAttribute("success","注册成功");
+        }
+        return "index";
     }
 
 }
--- src/main/java/cn/scnu/springbootfinalhomework2024/service/UserService.java
+++ src/main/java/cn/scnu/springbootfinalhomework2024/service/UserService.java
@@ -2,10 +2,34 @@ package cn.scnu.springbootfinalhomework2024.service;
 
 import cn.scnu.springbootfinalhomework2024.entity.User;
 import cn.scnu.springbootfinalhomework2024.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService extends ServiceImpl<UserMapper, User> {
 
+    UserMapper userMapper;
+
+    public User ConFirmLogin(User user) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("username", user.getUsername());
+        List<User> userList = userMapper.selectList(queryWrapper);
+        return userList.size() == 0 ? null : userList.get(0);
+    }
+
+    public String register(User user) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("phoneNumber", user.getPhoneNumber());
+        List<User> UserList = userMapper.selectList(queryWrapper);
+        if(UserList.size() == 0){
+            userMapper.insert(user);
+            return "注册成功";
+        }
+        else{
+            return "该手机号已被注册";
+        }
+    }
 }
